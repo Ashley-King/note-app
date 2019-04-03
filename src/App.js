84a1a8a4adef38c6c839c17a1df0e56f16adea89
Ashley King <ashleyot@gmail.com>
2019-04-03 15:06:56 -0400

    hitting rails notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
-import List from './components/List'
-import Note from './components/Note'
+import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
+import axios from 'axios';
 
 class App extends Component {
   constructor(){
@@ -17,13 +17,20 @@ class App extends Component {
       showNote: ! this.setState.showNote
     });
   }
+
+getNotes = () => {
+  axios.get('https://note-api-ashley.herokuapp.com/notes')
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err.response.data));
+}
+
   render() {
     const { showNote } = this.state;
     return (
       <div className="App">
          <Nav toggleNote={this.toggleNote} showNote={showNote}/>
          
-         {showNote ? <Note/> : <List/>}
+         {showNote ? <Note/> : <List getNotes={this.getNotes}/>}
          
          
       </div>
